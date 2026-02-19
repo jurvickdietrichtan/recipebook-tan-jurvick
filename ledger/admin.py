f10770eae5efc74ab6078cd39fae07afd5e484da
jurvickdietrichtan <jurvick.dietrich.tan@student.ateneo.edu>
2026-02-19 16:04:38 +0800

    lab2 admin.py

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient, RecipeIngredient
 
-# Register your models here.
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+    search_fields = ['name']
+    list_display = ['name']
+    inlines = [RecipeIngredientInline]
+
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
+    search_fields = ['name']
+    list_display = ['name']
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
