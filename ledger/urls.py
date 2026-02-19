cb5f01300f8d12871d724351c6d3cff377875b7c
jurvickdietrichtan <jurvick.dietrich.tan@student.ateneo.edu>
2026-02-19 15:46:42 +0800

    I FIXED IT HUAHAIHAIHAUIHAUIAHUIAHUIAHIAHIA

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import index, RecipeDetailView, recipe_detail, RecipeListView, recipe_list
+from django.urls import path
+from .views import recipe_list, recipe_detail
 
 urlpatterns = [
-    path('recipes/list', RecipeListView.as_view, name='recipe_list'),
-    path('recipe/<int:pk>', RecipeDetailView.as_view, name='recipe_detail'),
+    path('recipes/list', recipe_list, name='recipe_list'),
+    path('recipe/<int:pk>', recipe_detail, name='recipe_detail'),
 ]
+
+
+#from .views import index, RecipeDetailView, recipe_detail, RecipeListView, recipe_list
+
+#urlpatterns = [
+#    path('recipes/list', RecipeListView.as_view(), name='recipe_list'),
+#    path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe_detail'),
+#]
