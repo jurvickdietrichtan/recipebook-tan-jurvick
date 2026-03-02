45cffc73730c4876d473b14280f89edd162c00bd
jurvickdietrichtan <jurvick.dietrich.tan@student.ateneo.edu>
2026-03-02 17:22:08 +0800

    added login stuff

--- ledger/views.py
+++ ledger/views.py
@@ -7,6 +7,13 @@ from django.http import HttpResponse
 from .models import Recipe
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def recipe_detail(request, pk):
+    recipe = Recipe.objects.get(pk=pk)
+    context = {"recipe": recipe}
+    return render(request, "ledger/recipe_show.html", context)
 
 def index(request):
     return HttpResponse('Sorry bro.')
@@ -16,11 +23,6 @@ def recipe_list(request):
     context = {"recipes": recipes}
     return render(request, "ledger/recipe_list.html", context)
 
-
-def recipe_detail(request, pk):
-    recipe = Recipe.objects.get(pk=pk)
-    context = {"recipe": recipe}
-    return render(request, "ledger/recipe_detail.html", context)
 
 
 
--- recipebook/settings.py
+++ recipebook/settings.py
@@ -116,3 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+LOGIN_REDIRECT_URL = "recipe_list"
+
+LOGOUT_REDIRECT_URL = "login"
+
+LOGIN_URL = "login"
