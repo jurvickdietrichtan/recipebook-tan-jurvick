34c331c5db8dbd49d4fbdad1cab4a5735022241f
jurvickdietrichtan <jurvick.dietrich.tan@student.ateneo.edu>
2026-03-02 16:57:35 +0800

    add profile model

--- ledger/models.py
+++ ledger/models.py
@@ -1,6 +1,20 @@
+
+from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
 
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="profile"
+    )
+    name = models.CharField(max_length=50)
+    short_bio = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 
 class Ingredient(models.Model):
