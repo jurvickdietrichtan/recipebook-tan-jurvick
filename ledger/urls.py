a6ef28146ec2383cb9272d47eb1e2deb596b807a
jurvickdietrichtan <jurvick.dietrich.tan@student.ateneo.edu>
2026-03-12 15:49:51 +0800

    added urls and changed recipe_list

--- ledger/urls.py
+++ ledger/urls.py
@@ -5,6 +5,7 @@ from .views import recipe_list, recipe_detail
 urlpatterns = [
     path('recipes/list', recipe_list, name='recipe_list'),
     path('recipe/<int:pk>', recipe_detail, name='recipe_detail'),
+    path("recipe/add/", views.RecipeCreateView.as_view(), name="recipe_add")
 ]
 
 
--- ledger/views.py
+++ ledger/views.py
@@ -37,3 +37,4 @@ class RecipeCreateView(LoginRequiredMixin, CreateView):
 
 
 
+
