14c44bd8f56bbcccf82c39c2b685d24a3dc7a97f
jurvickdietrichtan <jurvick.dietrich.tan@student.ateneo.edu>
2026-03-12 16:42:41 +0800

    added return to recipe from add image and fixed forms stuff

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.urls import path
+from . import views
 from .views import recipe_list, recipe_detail
 
 urlpatterns = [
     path('recipes/list', recipe_list, name='recipe_list'),
     path('recipe/<int:pk>', recipe_detail, name='recipe_detail'),
-    path("recipe/add/", views.RecipeCreateView.as_view(), name="recipe_add")
+    path("recipe/add/", views.RecipeCreateView.as_view(), name="recipe_add"),
     path("recipe/<int:pk>/add_image/", views.RecipeImageCreateView.as_view(), name="add_image"),
     ]
 
--- ledger/views.py
+++ ledger/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.urls import reverse_lazy
+
 
 
 from django.http import HttpResponse
 from django.views.generic import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Recipe, RecipeImage
+from django import forms
 
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
@@ -34,10 +37,30 @@ class RecipeCreateView(LoginRequiredMixin, CreateView):
     fields = ["name"]
     template_name = "ledger/recipe_add.html"
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user.profile
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse_lazy("recipe_detail", kwargs={"pk": self.kwargs["pk"]})
+
 class RecipeImageCreateView(LoginRequiredMixin, CreateView):
     model = RecipeImage
     fields = ["image", "description"]
     template_name = "ledger/add_image.html"
+
+    def form_valid(self, form):
+        form.instance.recipe_id = self.kwargs["pk"]
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse_lazy("recipe_detail", kwargs={"pk": self.kwargs["pk"]})
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['recipe_pk'] = self.kwargs['pk']
+        return context
+
 
 
 
